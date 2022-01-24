877c94867029d1cd00bfbad4260270f9818ad93d
Y.Z.Y <jimmy_777@126.com>
2022-01-25 00:31:17 +0800

    springboot-jdbc例子实现CURD

---
+++ src/main/java/io/abc/springboot/controllers/BookController.java
@@ -0,0 +1,46 @@
+package io.abc.springboot.controllers;
+
+import io.abc.springboot.models.Book;
+import io.abc.springboot.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Array;
+import java.util.List;
+
+@RestController
+public class BookController {
+
+    // TODO: 2022/1/25
+
+    @Autowired
+    BookService bookService;
+
+    @GetMapping("/bookOps")
+    public void bookOps() {
+        Book book1 = new Book();
+        book1.setName("name-a1");
+        book1.setAuthor("author-a1");
+
+        int i = bookService.addBook(book1);
+        System.out.println("add new book >>> " + i);
+
+        Book book2 = new Book();
+        book2.setId(1);
+        book2.setName("name-a1-b1");
+        book2.setAuthor("author-a1-b1");
+        int i2 = bookService.updateBook(book2);
+        System.out.println("update book >>> " + i2);
+
+        Book book = bookService.getBookById(1);
+        System.out.println("get book >>> " + book);
+
+        List<Book> allBooks = bookService.getAllBooks();
+        System.out.println("get all books >>> " + allBooks);
+
+        int deleteBook = bookService.deleteBook(1);
+        System.out.println("delete book >>> " + deleteBook);
+
+    }
+}
