79d3d1534f8e9ee92d1d4f52abf536df66cc4f87
Y.Z.Y <jimmy_777@126.com>
2022-01-24 23:42:52 +0800

    清理干净项目文件。

--- src/main/java/io/abc/springboot/SpringbootApplication.java
+++ src/main/java/io/abc/springboot/SpringbootApplication.java
@@ -2,13 +2,9 @@ package io.abc.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.jms.annotation.EnableJms;
 
-import javax.sql.DataSource;
+@SpringBootApplication
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@EnableJms
 public class SpringbootApplication {
 
     public static void main(String[] args) {
