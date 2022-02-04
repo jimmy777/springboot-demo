2f49814787034920a2f30fcc9385f0dadc4d2e2f
Y.Z.Y <jimmy_777@126.com>
2022-02-04 14:08:06 +0800

    自定义用户的roles角色

--- src/main/java/io/abc/springboot/MyWebSecurityConfig.java
+++ src/main/java/io/abc/springboot/MyWebSecurityConfig.java
@@ -3,6 +3,7 @@ package io.abc.springboot;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,5 +22,19 @@ public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication().withUser("admin").password("123").roles("ADMIN", "USER")
                 .and()
                 .withUser("a1").password("123").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/hello")
+                .hasRole("ADMIN")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin().loginProcessingUrl("/login").permitAll()
+                .and()
+                .csrf()
+                .disable();
     }
 }
