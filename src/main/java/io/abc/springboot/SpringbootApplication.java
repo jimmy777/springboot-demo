7d465e8fa892ab95f0daffeded3f164ec9a84238
Y.Z.Y <jimmy_777@126.com>
2022-02-07 17:14:05 +0800

    springboot使用slf4j-log4j日志

--- src/main/java/io/abc/springboot/SpringbootApplication.java
+++ src/main/java/io/abc/springboot/SpringbootApplication.java
@@ -1,5 +1,7 @@
 package io.abc.springboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class SpringbootApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(SpringbootApplication.class);
+
     public static void main(String[] args) {
+        logger.info("============== 项目启动 =============");
         SpringApplication.run(SpringbootApplication.class, args);
+        logger.info("============== 项目停止 =============");
     }
 
 }
--- src/test/java/io/abc/springboot/SpringbootApplicationTests.java
+++ src/test/java/io/abc/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,17 @@
 package io.abc.springboot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class SpringbootApplicationTests {
+
+//    private static final Logger logger = LoggerFactory.getLogger(SpringbootApplication.class);
 
     @Test
     void contextLoads() {
+        log.info("带参日志测试:{}", "参数111");
     }
-
 }
