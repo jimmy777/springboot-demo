7747f8b239a87e65d5866288dc08305271d848fc
Y.Z.Y <jimmy_777@126.com>
2022-02-15 22:24:10 +0800

    sprigboot-jdbc的使用例子

--- src/main/java/io/abc/springboot/SpringbootApplication.java
+++ src/main/java/io/abc/springboot/SpringbootApplication.java
@@ -2,13 +2,24 @@ package io.abc.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootApplication
 
 public class SpringbootApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringbootApplication.class, args);
+    public static void main(String[] args) throws SQLException {
+        ConfigurableApplicationContext context = SpringApplication.run(SpringbootApplication.class, args);
+        DataSource dataSource = context.getBean(DataSource.class);
+        System.out.println(dataSource.getClass().getName());
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection.getCatalog());
+        System.out.println(context.getBean(JdbcTemplate.class));
     }
 
 }
